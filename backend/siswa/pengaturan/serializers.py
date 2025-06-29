c1601cea42f6feeedd00818d5a8d85321dd82bae
Kholil345 <63179427+Kholil345@users.noreply.github.com>
2025-06-29 11:06:43 +0700

    Delete backend/siswa/pengaturan directory

--- backend/siswa/pengaturan/serializers.py
+++
@@ -1,8 +0,0 @@
-# pengaturan/serializers.py
-from rest_framework import serializers
-from .models import PengaturanPendaftaran
-
-class PengaturanPendaftaranSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PengaturanPendaftaran
-        fields = ['is_open']
