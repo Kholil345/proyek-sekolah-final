29f09558ca4890e893ad545480b2ed1f371fb730
Kholil345 <63179427+Kholil345@users.noreply.github.com>
2025-06-29 11:05:49 +0700

    Delete backend/siswa/notifikasi directory

--- backend/siswa/notifikasi/serializers.py
+++
@@ -1,9 +0,0 @@
-# notifikasi/serializers.py
-
-from rest_framework import serializers
-from .models import Notifikasi
-
-class NotifikasiSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notifikasi
-        fields = ['id', 'title', 'content', 'is_read', 'timestamp']
