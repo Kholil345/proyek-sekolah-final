8735c797e829dca51514860a8b6fc8de9b6f2f24
Kholil345 <63179427+Kholil345@users.noreply.github.com>
2025-06-29 11:07:02 +0700

    Delete backend/siswa/pengelolaSiswa directory

--- backend/siswa/pengelolaSiswa/asgi.py
+++
@@ -1,8 +0,0 @@
-# nama_proyek/asgi.py
-
-import os
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nama_proyek.settings')
-
-application = get_asgi_application()
